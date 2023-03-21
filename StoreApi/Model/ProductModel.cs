ad056c0ed2646601bb56876d6866df00ac3a9634
Hüsniye ÇAPANOĞLU <85956625+capanoglu-hus@users.noreply.github.com>
2023-03-21 14:12:32 +0300

    Add Models

---
+++ StoreApi/Model/ProductModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreApi.Model
+{
+    public class ProductModel
+    {
+       
+     
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+
+        public Boolean IsApproved { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public int Category_Id { get; set; }
+
+
+
+        public int CreateUserId { get; set; }
+
+
+        public int UpdateUserId { get; set; }
+
+    }
+}
