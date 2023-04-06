86422f88cfacf4d046b10862e840b16476823ab6
Hüsniye ÇAPANOĞLU <85956625+capanoglu-hus@users.noreply.github.com>
2023-04-06 03:21:36 +0300

    storeapp

---
+++ storeapp/src/App.js
@@ -0,0 +1,19 @@
+
+
+import React from "react";
+import './App.css';
+
+import RouterPage from "./components/pages/RouterPage";
+
+function App() {
+  
+
+  
+    return (
+    <RouterPage />
+    );
+  }
+  
+
+
+export default App;
