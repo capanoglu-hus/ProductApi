a677c8ee961f8d8367aa3f3611ffb1d83dccdefc
Hüsniye ÇAPANOĞLU <85956625+capanoglu-hus@users.noreply.github.com>
2023-04-10 13:44:50 +0300

    Delete StoreApi directory

--- StoreApi/Model/UserLoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StoreApi.Model
-{
-    public class UserLoginModel
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
