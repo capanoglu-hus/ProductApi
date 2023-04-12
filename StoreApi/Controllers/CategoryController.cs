56351375a50e31d80991876edcf4b6e46c64e97a
Hüsniye ÇAPANOĞLU <85956625+capanoglu-hus@users.noreply.github.com>
2023-04-12 13:06:15 +0300

    Update CategoryController.cs

--- StoreApi/Controllers/CategoryController.cs
+++ StoreApi/Controllers/CategoryController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +13,7 @@ namespace StoreApi.Controllers
     public class CategoryController : ControllerBase
     {
         public static CategoryModel category = new CategoryModel();
+      
         private readonly DataContext _dataContext;
 
         public CategoryController(DataContext dataContext)
@@ -34,7 +35,6 @@ namespace StoreApi.Controllers
         {
             category.Name = request.Name;
             category.Description = request.Description;
-           
             category.Status = request.Status;
             category.CreatedDate = request.CreatedDate;
             category.UpdatedDate = request.UpdatedDate;
@@ -49,11 +49,42 @@ namespace StoreApi.Controllers
 
         [HttpPatch]
         [Route("UpdateCategory/{id}")]
-        public async Task<Category> UpdateCategory(Category objCategory)
+        public async Task<ActionResult<Category>> UpdateCategory(int id, Category request)
         {
-            _dataContext.Entry(objCategory).State = EntityState.Modified;
-            await _dataContext.SaveChangesAsync();
-            return objCategory;
+            var category = await _dataContext.Categorys.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            category.Name = request.Name;
+            category.Description = request.Description;
+            category.Status = request.Status;
+            category.UpdatedDate = request.UpdatedDate;
+            category.CreateUserId = request.CreateUserId;
+            category.UpdateUserId = request.UpdateUserId;
+
+            _dataContext.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _dataContext.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                ModelState.AddModelError("", "Unable to save change. " +
+                        "Try Again, if you have problem persists, " +
+                        "Contact your system administrator");
+            }
+
+            return Ok(category);
         }
 
         [HttpDelete]
@@ -71,3 +102,4 @@ namespace StoreApi.Controllers
         }
     }
 }
+
